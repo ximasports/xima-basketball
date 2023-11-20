0f2ad2faf89b5c5617f3ca0f4efef1c953bb3f1e
Wassim Slimane <wassim.slimane01@gmail.com>
2023-11-20 17:08:37 +0100

    Setup App file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('App first render', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
